e41a57df2b52e599a90af4765dd61bafa5c80d91
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-15 15:13:46 +0530

    added basic styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default async function Home() {
   const database = await fetch(
     `https://daily-beige.cmd.outerbase.io/getDatabase`,
@@ -9,29 +7,41 @@ export default async function Home() {
         "content-type": "application/json",
       },
       cache: "no-cache",
-      next: { revalidate: 1 },
+      next: { revalidate: 10 },
     }
   );
   const data = await database.json();
-  const { results, next_cursor, has_more } = data;
-  console.log(data, "DATA");
+  const { results } = data;
   if (!data) {
     return <div>Loading...</div>;
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <ul>
-        {results.map((item: any) => (
-          <div key={item.id} className="flex w-[90vw] justify-between">
-            <h3>{item.properties.Task.title[0].plain_text}</h3>
-            <p>Status: {item.properties.Status.status.name}</p>
-            <p>
-              Note: {item.properties.Note.rich_text[0]?.plain_text ?? "None"}
-            </p>
-          </div>
-        ))}
-      </ul>
+    <main className="flex min-h-screen flex-col items-center text-center justify-center p-24">
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th className="px-4 py-2">Task</th>
+            <th className="px-4 py-2">Status</th>
+            <th className="px-4 py-2">Note</th>
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((item: any) => (
+            <tr key={item.id} className="border-b">
+              <td className="px-4 py-2">
+                {item.properties.Task.title[0].plain_text}
+              </td>
+              <td className="px-4 py-2">
+                {item.properties.Status.status.name}
+              </td>
+              <td className="px-4 py-2">
+                {item.properties.Note.rich_text[0]?.plain_text ?? ""}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 }
