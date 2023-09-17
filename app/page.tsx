515ca28503f0eca618917c7e9f54cbe7ace54bc7
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-17 23:42:24 +0530

    added heading on main page

--- app/page.tsx
+++ app/page.tsx
@@ -16,31 +16,37 @@ export default async function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center text-center justify-center p-24">
-      <table className="table-auto">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Task</th>
-            <th className="px-4 py-2">Status</th>
-            <th className="px-4 py-2">Note</th>
-          </tr>
-        </thead>
-        <tbody>
-          {results.slice().reverse().map((item: any) => (
-            <tr key={item.id} className="border-b">
-              <td className="px-4 py-2">
-                {item.properties.Task.title[0].plain_text}
-              </td>
-              <td className="px-4 py-2">
-                {item.properties.Status.status.name}
-              </td>
-              <td className="px-4 py-2">
-                {item.properties.Note.rich_text[0]?.plain_text ?? ""}
-              </td>
+    <>
+      <main className="flex min-h-screen flex-col items-center text-center justify-center p-24">
+        <h1 className="font-bold text-xl mb-10">Submission Checklist </h1>
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Task</th>
+              <th className="px-4 py-2">Status</th>
+              <th className="px-4 py-2">Note</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </main>
+          </thead>
+          <tbody>
+            {results
+              .slice()
+              .reverse()
+              .map((item: any) => (
+                <tr key={item.id} className="border-b">
+                  <td className="px-4 py-2">
+                    {item.properties.Task.title[0].plain_text}
+                  </td>
+                  <td className="px-4 py-2">
+                    {item.properties.Status.status.name}
+                  </td>
+                  <td className="px-4 py-2">
+                    {item.properties.Note.rich_text[0]?.plain_text ?? ""}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </main>
+    </>
   );
 }
